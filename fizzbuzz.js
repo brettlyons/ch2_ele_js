ce3ad2553522cbb5d29e58a84067127fc87da560
Brett Lyons <blyons@students.naropa.edu>
2015-07-24 06:19:15 -0600

    Rewrote if/else if/else logic with ternary ops

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,10 +2,10 @@
 var fizzBuzz = function (nBound) {
   var output = "";
   for ( var i = 1 ; i <= nBound ; i++ ) {
-    if ( i % 15 == 0 ) { output += "FizzBuzz\n"; }
-    else if ( i % 5 == 0 ) { output += "Buzz\n"; }
-    else if ( i % 3 == 0 ) { output += "Fizz\n"; }
-    else { output += String(i + "\n"); }
+    ( i % 15 == 0 ) ? ( output += "FizzBuzz\n" ) :
+    ( i % 5 == 0 ) ?  ( output += "Buzz\n" ) :
+    ( i % 3 == 0 ) ?  ( output += "Fizz\n" ) :
+    ( output += String(i + "\n") )
   }
 return output;
 }
